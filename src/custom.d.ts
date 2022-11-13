f4ae89ca311670c71e4009d6d316864f05d2bf52
Maksim Lavrenyuk <maksim.lavrenyuk@outlook.com>
2022-11-13 22:38:10 +0300

    lost files were added

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -7,3 +7,8 @@ declare module '*.less' {
   const resource: {[key: string]: string};
   export = resource;
 }
+
+declare module '*.scss' {
+  const resource: {[key: string]: string};
+  export = resource;
+}
